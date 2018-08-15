73e087fedbf345dbb9864e6da175115bd91dde0c
ishidamu <users.noreply.github.com>
2018-08-15 16:41:37 +0900

    javaファイルをコンパイルし、実行環境にコピーできるように修正した

--- service-jndi/sample/SampleRemote.java
+++
@@ -1,3 +0,0 @@
-public interface SampleRemote extends java.rmi.Remote{
-    public String getMessage() throws java.rmi.RemoteException;
-}
--- service-jndi/sample/SampleRemoteObject.java
+++
@@ -1,5 +0,0 @@
-public class SampleRemoteObject implements SampleRemote{
-    public String getMessage(){
-        return "test";
-    }
-}
